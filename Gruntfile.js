5907d34163b39d7697ee551c6d4cfb47af1f9980
Benjamin Longearet <bl@teads.tv>
2014-06-05 10:59:55 +0200

    Initial commit é

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+/*
+ * grunt-gzip
+ * https://github.com/firehist/grunt-gzip
+ *
+ * Copyright (c) 2014 Benjamin Longearet
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    gzip: {
+      default_options: {
+        files: {
+          'tmp/gzip.gz': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      }
+    },
+    deflate: {
+      default_options: {
+        files: {
+          'tmp/deflate': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      }
+    },
+    deflateRaw: {
+      default_options: {
+        files: {
+          'tmp/deflateRaw': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'gzip', 'deflate', 'deflateRaw', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/gzip.js
@@ -0,0 +1,70 @@
+/*
+ * grunt-gzip
+ * https://github.com/firehist/grunt-gzip
+ *
+ * Copyright (c) 2014 Benjamin Longearet
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  var fs = require('fs');
+  var zlib = require('zlib');
+
+  /**
+   * Generic method to provide a zlib[method] action
+   *  * Should be executed in a grunt task context *
+   * @param {string} method
+   */
+  var processAction = function(method) {
+    // Create async task
+    var done = this.async();
+    // Manage countdown to know when done task
+    var filesNum = this.files.length;
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf("\n"));
+
+      // Launch gzip action
+      zlib[method](src, function(err, res) {
+        if (err) {
+          grunt.log.error(err);
+        }
+        // countdown to make this task DONE
+        if (!--filesNum) {
+          done();
+        }
+        // Write the destination file.
+        grunt.file.write(f.dest, res);
+        // Print a success message.
+        grunt.log.writeln('[' + method + '] File "' + f.dest + '" created.');
+      });
+
+    });
+  };
+
+  // http://nodejs.org/api/zlib.html#zlib_class_zlib_gzip
+  var availableMethod = ['gzip', 'deflate', 'deflateRaw'];
+
+  // Programaticaly create all multiTask called as in availableMethod
+  availableMethod.forEach(function (method) {
+    grunt.registerMultiTask(method, 'Grunt task to manage ' + method, function() {
+      processAction.call(this, method);
+    });
+  });
+
+};
